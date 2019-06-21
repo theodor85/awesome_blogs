63986308babe3e8c36e52a17047a58f9235ab908
Teddy Coder <teddy_coder@mail.ru>
2019-06-21 13:17:28 +0700

    Добавлена модель поста

--- awesome_blogs/blogs/models.py
+++ awesome_blogs/blogs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    ''' Модель сущности постов пользователей. '''
+
+    content = models.TextField(verbose_name='Текст поста')
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True,
+        verbose_name='Опубликовано')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-created_at']
+    
